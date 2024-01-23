033841ad448dac676b3a3c724b127559ddf0bf17
Sadman <sadman.hossain@mail.utoronto.ca>
2024-01-22 19:45:15 -0500

    Add imports and placeholders for KVClient functions

--- m1-stub/src/app_kvClient/KVClient.java
+++ m1-stub/src/app_kvClient/KVClient.java
@@ -1,16 +1,31 @@
 package app_kvClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.UnknownHostException;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import logger.LogSetup;
+
 import client.KVCommInterface;
+import client.KVStore;
 
 public class KVClient implements IKVClient {
+    private KVStore kvstore = null; 
+
     @Override
     public void newConnection(String hostname, int port) throws Exception{
-        // TODO Auto-generated method stub
+        // TODO Auto-generated method stub ///
+        kvstore = new KVStore(hostname, port);
+        kvstore.connect();
     }
 
     @Override
     public KVCommInterface getStore(){
         // TODO Auto-generated method stub
-        return null;
+        return kvstore;
     }
 }
