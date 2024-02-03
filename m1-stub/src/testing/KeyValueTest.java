4da18c9506668c945079f02fdeb9ea793804ff0f
Sadman <sadman.hossain@mail.utoronto.ca>
2024-02-03 14:45:10 -0500

    Add tests for `put`/`get` with key-value pair combinations

---
+++ m1-stub/src/testing/KeyValueTest.java
@@ -0,0 +1,233 @@
+package testing;
+
+import org.junit.Test;
+
+import client.KVStore;
+import junit.framework.TestCase;
+import shared.messages.KVMessage;
+import shared.messages.KVMessage.StatusType;
+
+
+public class KeyValueTest extends TestCase {
+
+	private KVStore kvClient;
+	
+	public void setUp() {
+		kvClient = new KVStore("localhost", 50000);
+		try {
+			kvClient.connect();
+		} catch (Exception e) {
+		}
+	}
+
+	public void tearDown() {
+		kvClient.disconnect();
+	}
+	
+	
+	@Test
+	public void testPutEmptyKey() {
+		String key = "";
+		String value = "some_val";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.put(key, value);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_SUCCESS);
+	}
+	
+	@Test
+	public void testPutEmptyValue() {
+		String key = "some_key";
+		String value = "";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.put(key, value);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_SUCCESS);
+	}
+	
+	@Test
+	public void testPutEmptyKeyAndValue() {
+		String key = "";
+		String value = "";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.put(key, value);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_SUCCESS);
+	}
+	
+	@Test
+	public void testPutKeyWithSpaces() {
+		String key = "key with spaces";
+		String value = "some_val";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.put(key, value);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_SUCCESS);
+	}
+	
+	@Test
+	public void testGetKeyWithSpaces() {
+		String key = "key with spaces";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.get(key);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_SUCCESS);
+	}
+	
+	@Test
+	public void testPutKeyWithNewlines() {
+		String key = "key \n with \n newlines";
+		String value = "some_val";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.put(key, value);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_SUCCESS);
+	}
+	
+	@Test
+	public void testGetKeyWithNewlines() {
+		String key = "key \n with \n newlines";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.get(key);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_SUCCESS);
+	}
+	
+	@Test
+	public void testPutValueWithNewlines() {
+		String key = "key \n with \n newlines";
+		String value = "some_val";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.put(key, value);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_SUCCESS);
+	}
+	
+	@Test
+	public void testPutKeyAndValueWithNewlines() {
+		String key = "key \n with \n newlines";
+		String value = "value \n with \n newlines";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.put(key, value);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_SUCCESS);
+	}
+	
+	@Test
+	public void testPutKeyMaxLen() {
+		String key = "a".repeat(20);
+		String value = "some_val";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.put(key, value);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_SUCCESS);
+	}
+	
+	@Test
+	public void testPutKeyTooLong() {
+		String key = "a".repeat(21);
+		String value = "some_val";
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.put(key, value);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertNull(ex);
+		assertEquals(KVMessage.StatusType.PUT_ERROR, response.getStatus());
+	}
+	
+	@Test
+	public void testGetKeyMaxLen() {
+		String key = "a".repeat(20);
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.get(key,);
+		} catch (Exception e) {
+			ex = e;
+		}
+		
+		assertTrue(ex == null && response.getStatus() == StatusType.PUT_SUCCESS);
+	}
+	
+	@Test
+	public void testGetKeyTooLong() {
+		String key = "a".repeat(21);
+		KVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.get(key,);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertNull(ex);
+		assertEquals(KVMessage.StatusType.PUT_ERROR, response.getStatus());
+	}
+}
