ed49c508a57448ebc417740a22ca2954baf5633a
Sean Kim <kimsihy093@gmail.com>
2024-02-04 01:25:14 -0500

    update testGetKeyWithNewlines

--- m1-stub/src/shared/messages/BasicKVMessage.java
+++ m1-stub/src/shared/messages/BasicKVMessage.java
@@ -42,7 +42,11 @@ public class BasicKVMessage implements KVMessage{
   }
 
   public String getValue() {
-    return value;
+      return value;
+  }
+
+  public void changeKey(String key) {
+    this.key = key;
   }
 
   public StatusType getStatus() {
