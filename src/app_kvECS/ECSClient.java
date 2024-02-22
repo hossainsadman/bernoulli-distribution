b2ae8538c3373334c628915904404787bd64e9a0
Sadman <sadman.hossain@mail.utoronto.ca>
2024-02-22 11:47:14 -0500

    Add basic cli parameters to `ECSClient.java`
    Update `build.xml` to build `m2-ecs.jar`

--- src/app_kvECS/ECSClient.java
+++ src/app_kvECS/ECSClient.java
@@ -1,11 +1,37 @@
 package app_kvECS;
 
+import java.io.*;
 import java.util.Map;
 import java.util.Collection;
 
 import ecs.IECSNode;
 
+import java.net.ServerSocket;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import logger.LogSetup;
+
+import org.apache.commons.cli.*;
+
 public class ECSClient implements IECSClient {
+
+	private static final int DEFAULT_ECS_PORT = 9999;
+
+    private String address;
+    private int port;
+
+    private ServerSocket ecsSocket;
+
+    private static Logger logger = Logger.getRootLogger();
+
+    public ECSClient(String address, int port) {
+        if (port < 1024 || port > 65535)
+            throw new IllegalArgumentException("port is out of range.");
+        
+        this.address = address;
+        this.port = port;
+    }
 
     @Override
     public boolean start() {
@@ -67,7 +93,63 @@ public class ECSClient implements IECSClient {
         return null;
     }
 
-    public static void main(String[] args) {
-        // TODO
+    public static void main(String[] args) throws IOException {
+        Options options = new Options();
+
+        Option help = new Option("h", "help", false, "display help");
+        help.setRequired(false);
+        options.addOption(help);
+
+        Option address = new Option("a", "address", true, "address to start ecs service");
+        address.setRequired(false);
+        options.addOption(address);
+
+        Option port = new Option("p", "port", true, "server port");
+        port.setRequired(false);
+        options.addOption(port);
+        
+        Option logFile = new Option("l", "logFile", true, "log file path");
+        logFile.setRequired(false);
+        options.addOption(logFile);
+
+        Option logLevel = new Option("ll", "logLevel", true, "log level");
+        logLevel.setRequired(false);
+        options.addOption(logLevel);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;    
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("m2-ecs.jar", options);
+            System.exit(1);
+            return;
+        }
+
+        if (cmd.hasOption("help")) {
+            formatter.printHelp("m2-ecs.jar", options);
+            System.exit(0);
+        }
+        
+        // set defaults for options
+        String ecsAddress = cmd.getOptionValue("address", "localhost");
+        String ecsPort = cmd.getOptionValue("port", String.valueOf(DEFAULT_ECS_PORT));
+        String ecsLogFile = cmd.getOptionValue("logFile", "logs/ecs.log");
+        String ecsLogLevel = cmd.getOptionValue("logLevel", "ALL");
+
+        if (!LogSetup.isValidLevel(ecsLogLevel)) {
+            ecsLogLevel = "ALL";
+        }
+        
+        try {
+            new LogSetup(ecsLogFile, LogSetup.getLogLevel(ecsLogLevel));
+            ECSClient ecsclient = new ECSClient(ecsAddress, Integer. parseInt(ecsPort));
+            logger.info("ECSClient started at " + address + ":" + port);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }        
     }
 }
