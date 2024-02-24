6074e3607cac30bc8be3bcd447a3e592af4644f0
Sean Kim <kimsihy093@gmail.com>
2024-02-23 21:08:56 -0500

    create M2Test.java

---
+++ src/testing/M2Test.java
@@ -0,0 +1,23 @@
+package testing;
+
+import java.net.UnknownHostException;
+
+import app_kvServer.KVServer;
+
+import junit.framework.TestCase;
+
+/* 
+    Integration of the JUnit library into your project (as in Milestone 1)
+    Automate the running of the given test cases Do regression testing, 
+    make sure that relevant Milestone 1 tests still pass Test functionality such as create connection/disconnect,get/put value, update value (existing key), get non-existing key(check error messages)
+
+    Add at least 10 test cases of your choice that cover the additional functionality and features of this milestone (e.g., ECS,consistent hashing, metadata updates, retry operations, locks ,etc.)
+
+    Compile a short test report about all test cases, especially your own cases (submit as appendix of your design document).
+*/
+
+public class M2Test extends TestCase {
+    public void testStubM2() {
+        return;
+    }
+}
